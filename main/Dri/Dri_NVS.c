b59ec53409f71fdb140d65bf226a2c3de2b1acb1
ZhangYY <1443802862@qq.com>
2025-12-09 13:46:15 +0800

    flash存储测试

--- main/Dri/Dri_NVS.c
+++ main/Dri/Dri_NVS.c
@@ -6,7 +6,7 @@ nvs_handle_t flash_handle;
 void Dri_NVS_Init(void)
 {
     esp_err_t err = nvs_flash_init();
-    if(err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
         //* flash空间不足
 
@@ -18,8 +18,8 @@ void Dri_NVS_Init(void)
     }
 
     //* 在flash空间上申请空间(可写可读)
-    esp_err_t result = nvs_open("stroage",NVS_READWRITE,&flash_handle);
-    if(result == ESP_OK)
+    esp_err_t result = nvs_open("stroage", NVS_READWRITE, &flash_handle);
+    if (result == ESP_OK)
     {
         printf("flash space has been successfully created!\r\n");
     }
@@ -36,21 +36,21 @@ esp_err_t Dri_NVS_SetStr(char *key, char *value)
 }
 
 //* NVS读取字符串 -- 用于读取管理员密码
-esp_err_t Dri_NVS_GetStr(char *key, char *buffer, size_t sizes)
+esp_err_t Dri_NVS_GetStr(char *key, char *buffer, size_t *sizes)
 {
-    return nvs_get_str(flash_handle,key,buffer,sizes);
+    return nvs_get_str(flash_handle, key, buffer, sizes);
 }
 
 //* NVS写入数字 -- 用于存储普通用户密码
 esp_err_t Dri_NVS_SetU8(char *key, uint8_t *value)
 {
-    return nvs_set_u8(flash_handle,key,value);
+    return nvs_set_u8(flash_handle, key, value);
 }
 
 //* NVS读取数字 -- 用于读取普通用户密码
 esp_err_t Dri_NVS_GetU8(char *key, uint8_t *buffer)
 {
-    return nvs_get_u8(flash_handle,key,buffer);
+    return nvs_get_u8(flash_handle, key, buffer);
 }
 
 //* 删除所有密码
@@ -62,11 +62,11 @@ esp_err_t Dri_NVS_DelAll(void)
 //* 根据key删除指定密码
 esp_err_t Dri_NVS_DelByKey(char *key)
 {
-    return nvs_erase_key(flash_handle,key);
+    return nvs_erase_key(flash_handle, key);
 }
 
 //* 检测密码是否存在
 esp_err_t Dri_NVS_KeyIsExist(char *key)
 {
-    return nvs_find_key(flash_handle,key,NULL);
+    return nvs_find_key(flash_handle, key, NULL);
 }
--- main/main.c
+++ main/main.c
@@ -10,6 +10,17 @@ void app_main(void)
     //* 初始化
     Dri_NVS_Init();
 
-    Dri_NVS_SetStr("ADMIN","123456");
-    
+    //* 存入管理员密码
+    Dri_NVS_SetStr("ADMIN", "123456");
+
+    char buffers[10] = {0};
+    size_t len = sizeof(buffers);
+
+    //* 获取管理员密码
+    esp_err_t result = Dri_NVS_GetStr("ADMIN", buffers, &len);
+
+    if (result == ESP_OK)
+    {
+        printf("admin ok : %s\r\n", buffers);
+    }
 }
