53d550e694c5c432b3ab170df8b11ea46b9ec630
ZhangYY <1443802862@qq.com>
2025-12-10 20:09:57 +0800

    完善 FPM383 模块的初始化和休眠功能，添加外部中断处理逻辑

--- main/Int/Int_FPM383.c
+++ main/Int/Int_FPM383.c
@@ -36,12 +36,86 @@ static void Int_FPM383_ReceiveAck(uint32_t length, TickType_t waitTime)
     uart_read_bytes(UART_NUM_1, receive_buffers, length, waitTime);
 }
 
+//* 指纹中断服务
+static void Int_FPM383_handler(void *pin)
+{
+    esp_rom_printf("touch down\r\n");
+    //* 切换标志位
+    finger_touch_down = 1;
+}
+
 //* FPM383 -- 初始化
 void Int_FPM383_Init(void)
 {
+    //* 串口相关配置
+    const uart_config_t uart_config = {
+        .baud_rate = 57600,                    // 波特率
+        .data_bits = UART_DATA_8_BITS,         // 数据位是8位
+        .parity = UART_PARITY_DISABLE,         // 禁用奇偶校验
+        .stop_bits = UART_STOP_BITS_1,         // 1位停止位
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE, // 禁用硬件流功能
+        .source_clk = UART_SCLK_DEFAULT        // 串口时钟源
+    };
+
+    uart_param_config(UART_NUM_1, &uart_config);
+
+    //* 开启串口使能
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+
+    //* 配置uart收发引脚
+    uart_set_pin(UART_NUM_1, FPM383_TX_PIN, FPM383_RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    //* 添加外部中断
+    gpio_config_t io_config = {
+        .intr_type = GPIO_INTR_POSEDGE, // 上升沿触发
+        .mode = GPIO_MODE_INPUT,        // 输入模式
+        .pin_bit_mask = (1 << FPM383_TOUCH_OUT),
+        .pull_down_en = GPIO_PULLDOWN_ENABLE, // 允许下拉
+        .pull_up_en = GPIO_PULLUP_DISABLE     // 禁用上拉
+    };
+
+    gpio_config(&io_config);
+
+    //* 配置中断优先级
+    gpio_install_isr_service(0);
+
+    //* 添加中断服务函数
+    gpio_isr_handler_add(FPM383_TOUCH_OUT, Int_FPM383_handler, (void *)FPM383_TOUCH_OUT);
+
+    //* 关闭外部中断
+    gpio_intr_disable(FPM383_TOUCH_OUT);
+
+    //* 延迟一段时间后再发送命令
+    vTaskDelay(150); //* 150ms
+
+    //* 进入休眠状态
+    Int_FPM383_Sleep();
 }
 
 //* FPM383 -- 进入休眠状态
 void Int_FPM383_Sleep(void)
 {
+    //* 定义命令数组
+    uint8_t cmds[12] = {
+        0xEF, 0x01,             // 包头
+        0xff, 0xff, 0xff, 0xff, // 设备地址
+        0x01,                   // 包标识
+        0x00, 0x03,             // 包长度
+        0x33,                   // 指令码
+        0x00, 0x37              // 校验和
+    };
+
+    //* 发送多次休眠命令,直到收到正确响应为止
+    do
+    {
+        //* 发送sleep命令
+        Int_FPM383_SendCmd(cmds, 12);
+        //* 接收应答数据
+        Int_FPM383_ReceiveAck(12, 1000);
+    } while (receive_buffers[9] != 0x00);
+
+    //* 开启中断
+    gpio_intr_enable(FPM383_TOUCH_OUT);
+
+    esp_rom_printf("fpm383 sleep!\r\n");
 }
