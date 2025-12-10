192c2c5c569c7211f953ac1bb4a2fa18e807cea0
ZhangYY <1443802862@qq.com>
2025-12-10 15:35:42 +0800

    添加 FPM383 传感器的头文件和源文件

---
+++ main/Int/Int_FPM383.c
@@ -0,0 +1,9 @@
+#include "Int_FPM383.h"
+
+void Int_FPM383_Init(void)
+{
+}
+
+void Int_FPM383_Sleep(void)
+{
+}
---
+++ main/Int/Int_FPM383.h
@@ -0,0 +1,16 @@
+#ifndef __INT_FPM383_H__
+#define __INT_FPM383_H__
+#include "rom/ets_sys.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include <string.h>
+
+#define FPM383_TX_PIN GPIO_NUM_21
+#define FPM383_RX_PIN GPIO_NUM_20
+#define FPM383_TOUCH_OUT GPIO_NUM_10
+
+void Int_FPM383_Init(void);
+
+void Int_FPM383_Sleep(void);
+
+#endif /* __INT_FPM383_H__ */
